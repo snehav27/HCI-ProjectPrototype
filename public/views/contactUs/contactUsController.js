287d2ac4a7e35256e671098dc7d650d09377787a
Sneha <vankireddy.s@husky.neu.edu>
2015-11-28 05:59:56 -0500

    contactUs and Friends

--- public/views/contactUs/contactUsController.js
+++ public/views/contactUs/contactUsController.js
@@ -4,6 +4,28 @@
     Details: contact us Controller for application.
     Email  : hci.cs5340@gmail.com
 */
-app.controller('contactUsCtrl', function ($scope, $rootScope, $location) {
+app.controller('contactUsCtrl', function ($scope, $rootScope, $location, $mdToast) {
+
+	$scope.isQueryEmpty =false;
+
+	$scope.sendQuery = function() {
+	if($scope.query != null) {
+
+		showToastMessage('Thanks for Contacting Us .. We will get back to you soon');
+		
+	} else {
+
+		$scope.isQueryEmpty =true;
+    }
+   }
+
+  function showToastMessage(message) {
+    $mdToast.show(
+      $mdToast.simple()
+        .content(message)
+        .position('top right')
+        .hideDelay(3000)
+    );
+  };
 
 });
--- public/views/myPhotos/myPhotosController.js
+++ public/views/myPhotos/myPhotosController.js
@@ -5,5 +5,26 @@
     Email  : hci.cs5340@gmail.com
 */
 app.controller('myPhotosCtrl', function ($scope, $rootScope, $location) {
-
+	$scope.myPhotoData = [{
+          'name'      : 'Woment Empowerment',
+          'date'       : '3-Feb-2016',
+          'description'  : 'Woment Empowerment for UCB members',
+          'location'     : 'Boston, MA',
+          'points'		 : '400'
+        },
+        {
+          'name'      : 'Voting information',
+          'date'       : '3-Feb-2016',
+          'description'  : 'Voting information for UCB members',
+          'location'     : 'Boston, MA',
+          'points'		 : '150'
+        },
+        {
+          'name'      : 'Flu shot',
+          'date'       : '3-Feb-2016',
+          'description'  : 'Flu shot for UCB members',
+          'location'     : 'Boston, MA',
+          'points'		 : '280'
+        }
+    ];
 });
